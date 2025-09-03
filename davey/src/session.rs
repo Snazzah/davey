ac795cd578dd7393d625ad11393f960301953668
Snazzah <me@snazzah.com>
2025-09-03 15:17:03 -0400

    chore: clippy, will ci work now???

--- davey/src/session.rs
+++ davey/src/session.rs
@@ -415,11 +415,10 @@ impl DaveSession {
 
               debug!("Storing add proposal for user {incoming_user_id}");
 
-              if let Some(ids) = expected_user_ids {
-                if !ids.contains(&incoming_user_id) {
+              if let Some(ids) = expected_user_ids
+                && !ids.contains(&incoming_user_id) {
                   return Err(ProcessProposalsError::UnexpectedUser(incoming_user_id));
                 }
-              }
 
               commit_adds_members = true;
             } else if let Proposal::Remove(remove_proposal) = proposal.proposal() {
